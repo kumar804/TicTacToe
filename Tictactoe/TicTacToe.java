71cc4a520edb9084bc13b9dea257b8f01bdc0b99
kumar804 <kumarsinghmanish547@gmail.com>
2021-07-16 09:40:55 +0530

    Created Board for TicTactoe game

--- Tictactoe/TicTacToe.java
+++ Tictactoe/TicTacToe.java
@@ -3,5 +3,14 @@ package Tictactoe;
 public class TicTacToe {
     public static void main(String[] args) {
         System.out.println("Welcome To TicTacToe");
+        char[] board = createBoard();
+    }
+    public static char[] createBoard(){
+        char[] board = new char[10];
+        for(int i = 1; i < board.length; i++)
+        {
+            board[i] = ' ';
+        }
+        return board;
     }
 }
