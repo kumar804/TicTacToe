cfb4310b1e42f86e9f02248851f0f0523734d5d9
kumar804 <kumarsinghmanish547@gmail.com>
2021-07-17 07:18:49 +0530

    created method showBoard

--- Tictactoe/TicTacToe.java
+++ Tictactoe/TicTacToe.java
@@ -3,28 +3,33 @@ package Tictactoe;
 import java.util.Scanner;
 
 public class TicTacToe {
-    char[] board = createBoard();
+
     public static void main(String[] args) {
         System.out.println("Welcome To TicTacToe");
-
+        char[] board = createBoard();
+        choose();
+        showBoard(board);
     }
-    /*craeting board for TicTacToe of size 10 and ignoring
+    /*
+    craeting board for TicTacToe of size 10 and ignoring
     0th index
      */
-    public static char[] createBoard() {
+    public static char[] createBoard()
+    {
         char[] board = new char[10];
-        for (int i = 1; i < board.length; i++) {
+        for(int i = 1; i < board.length; i++)
+        {
             board[i] = ' ';
         }
         return board;
     }
-        /*
-        creating a function to chose letter x or o if user chose x then computer variable will be
+    /*
+    creating a function to chose letter x or o if user chose x then computer variable will be
         x
-         */
+     */
     public static void choose()
     {
-        //takin user input to chose letter
+        //takin input from user so user can chose letter of his own choice
         Scanner sc = new Scanner(System.in);
         char computerVariable = 0 ;
         System.out.println("Enter the characater 'X' OR 'O' to play");
@@ -41,12 +46,23 @@ public class TicTacToe {
             System.out.println("Player input is :" +playerChoose);
             System.out.println("Computer input is :" +computerVariable);
         }
-
         else
         {
             System.out.println("Invalid input please input the valid charcter");
         }
 
-
+    }
+    /*
+    creating a function to show board of TicTacToe game at run
+    time
+     */
+    public static void showBoard(char[] board)
+    {
+        System.out.println(board[1] + " | " + board[2] + " | " + board[3]);
+        System.out.println("--------------");
+        System.out.println(board[4] + " | " + board[5] + " | " + board[6]);
+        System.out.println("--------------");
+        System.out.println(board[7] + " | " + board[8] + " | " + board[9]);
+        System.out.println("--------------");
     }
 }
