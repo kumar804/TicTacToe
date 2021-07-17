859163a4a8571ec5b6d22a464586f0f38433c56e
kumar804 <kumarsinghmanish547@gmail.com>
2021-07-17 08:31:56 +0530

    created a method to check winner

--- Tictactoe/TicTacToe.java
+++ Tictactoe/TicTacToe.java
@@ -5,16 +5,18 @@ import java.util.Scanner;
 public class TicTacToe {
 
     public static void main(String[] args) {
-        System.out.println("Welcome To TicTacToe");
         char[] board = createBoard();
         char choice = choose();
         char[] ticBoard = showBoard(board);
         toss();
-        //move(ticBoard , choice);
+        move(ticBoard , choice);
         //showBoard(board);
+        char win =winnerCheck(board);
+        System.out.println("Winner is " + win);
+
     }
     /*
-    craeting board for TicTacToe of size 10 and ignoring
+     craeting board for TicTacToe of size 10 and ignoring
     0th index
      */
     public static char[] createBoard()
@@ -32,6 +34,7 @@ public class TicTacToe {
      */
     public static char choose()
     {
+        // taking user input
         Scanner sc = new Scanner(System.in);
         char computerVariable = 0 ;
         System.out.println("Enter the characater 'X' OR 'O' to play");
@@ -75,20 +78,25 @@ public class TicTacToe {
     public static char[] move(char[] board , char choose)
     {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the value between 1 to 9 to move");
-        int value = sc.nextInt();
-        if(board[value] == ' ')
+        for(int i = 1; i <= 3; i++)
         {
-            board[value] = choose;
+            System.out.println("Enter the value between 1 to 9 to move");
+            int value = sc.nextInt();
+            if(board[value] == ' ')
+            {
+                board[value] = choose;
+                showBoard(board);
+            }
+            else
+            {
+                System.out.println("Your desired place is not empty ");
+            }
+            if(value < 1 || value > 9)
+            {
+                System.out.println("Invalid input please input the valid digit");
+            }
         }
-        else
-        {
-            System.out.println("Your desired place is not empty");
-        }
-        if(value < 1 || value > 9)
-        {
-            System.out.println("Invalid input please input the valid digit");
-        }
+        winnerCheck(board);
         return board;
     }
     /*
@@ -103,8 +111,68 @@ public class TicTacToe {
             case 1://Head
                 System.out.println("player 1 win the toss so player 1 play the first");
                 break;
-            case 2://Tail
+            default://Tail
                 System.out.println("computer win the toss so the computer play the first");
         }
+    }
+    /*
+    created method to check winner
+     */
+    public static char winnerCheck(char[] board)
+    {
+        for( int i = 1; i < 3; i++ )  // For Horizontally
+        {
+            for(int j = 1;j <= i; j = j+3)
+            {
+                if( ( board[j] == 'X') && ( board[i+j] == 'X') && ( board[i+j+1] == 'X' ) )
+                {
+                    return 'X';
+                }
+                if( ( board[j] == 'O' ) && ( board[i+j] == 'O' ) && ( board[i+j+1] == 'O' ) )
+                {
+                    return 'O';
+                }
+            }
+        }
+        for( int i = 1; i < 3; i++ )  // For Vertically
+        {
+            for(int j = 1;j <= i; j++)
+            {
+                if( ( board[j] == 'X' ) && ( board[j+i+2] == 'X' ) && ( board[j+i+5] == 'X' ) )
+                {
+                    return 'X';
+                }
+
+                if( ( board[j] == 'O' ) && ( board[j+i+2] == 'O' ) && ( board[j+i+5] == 'O' ) )
+                {
+                    return 'O';
+                }
+            }
+        }
+        // For Diagonal
+
+        if( ( board[1] == 'X' ) &&  ( board[5] == 'X' ) &&  ( board[9] == 'X' ) )
+            return 'X';
+        if( ( board[3] == 'X' ) &&  ( board[5] == 'X' ) &&  ( board[7] == 'X' ) )
+            return 'X';
+        if( ( board[1] == 'O' ) &&  ( board[5] == 'O' ) &&  ( board[9] == 'O' ) )
+            return 'O';
+        if( ( board[3] == 'O' ) &&  ( board[5] == 'O' ) &&  ( board[7] == 'O' ) )
+            return 'O';
+
+        //check for a cell ( no win )
+        for( int i = 1; i < board.length; i++ )
+        {
+            int  cell=0;
+            if(board[i]!=' ')
+            {
+                cell++;
+            }
+            if(cell==9)
+            {
+                System.out.println("Match is tie");
+            }
+        }
+        return 0;
     }
 }
